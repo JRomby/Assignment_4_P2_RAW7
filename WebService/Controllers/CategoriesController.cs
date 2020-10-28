a9ccbe7c4c7d4817d22f234da3da352fe5ba6241
Smedenergod <smedenergod@gmail.com>
2020-10-28 23:29:13 +0100

    Done all the basic requests

---
+++ WebService/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebService.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        DataService.DataService service = new DataService.DataService();
+
+        [HttpGet]
+        //http://localhost:5001/api/categories
+        [Route("api/categories")]
+        public IEnumerable<DataService.Categories> GetCategories()
+        {
+            return service.GetCategories().ToArray();
+        }
+
+        //http://localhost:5001/api/categories/1
+        [Route("api/categories/{id}")]
+        public DataService.Categories GetCategory(int id)
+        {
+            return service.GetCategory(id);
+        }
+
+        //http://localhost:5001/api/categories/create/smed+smed
+        [Route("api/categories/create/{name}+{description}")]
+        public DataService.Categories Details(string name,  string description)
+        {
+            return service.CreateCategory(name, description);
+        }
+
+        //http://localhost:5001/api/categories/delete/10
+        [Route("api/categories/delete/{id}")]
+        public bool DeleteCategory(int id)
+        {
+            return service.DeleteCategory(id);
+        }
+
+        [Route("api/categories/update/{id}+{name}+{description}")]
+        //http://localhost:5001/api/categories/update/9+smed2+smed2
+        public bool UpdateCategory(int id, string name, string description)
+        {
+            return service.UpdateCategory(id, name, description);
+        }
+    }
+}
---
+++ WebService/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebService.Controllers
+{
+    public class OrderController : Controller
+    {
+        DataService.DataService service = new DataService.DataService();
+
+        //http://localhost:5001/api/products/1
+        [Route("api/products/{id}")]
+        public DataService.Products GetProduct(int id)
+        {
+            return service.GetProduct(id);
+        }
+
+        //http://localhost:5001/api/products/category/1
+        [Route("api/products/category/{id}")]
+        public IEnumerable<DataService.Products> GetProductByCategory(int id)
+        {
+            return service.GetProductByCategory(id);
+        }
+
+        //http://localhost:5001/api/products/name/em
+        [Route("api/products/name/{name}")]
+        public IEnumerable<DataService.Products> GetProductByName(string name)
+        {
+            return service.GetProductByName(name);
+        }
+    }
+}
